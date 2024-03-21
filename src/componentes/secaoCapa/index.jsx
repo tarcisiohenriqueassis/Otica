374c5cd8622944553c4e4fe3e3494462dff62946
teste <tarcisiohenriqueassis@gmail.com>
2024-03-21 13:39:16 -0300

    adicionado imagens e feito topo e banner

---
+++ src/componentes/secaoCapa/index.jsx
@@ -0,0 +1,18 @@
+import estilos from '@/componentes/secaoCapa/banner.module.css';
+import banner from '../../../public/capa.png';
+import Image from 'next/image';
+
+export default function Banner(){
+    return(
+        <section className={estilos.container}>
+        <Image className={estilos.banner} src={banner} alt='Banner'/>
+        <div className={estilos.containerTitulo}>
+            <div className={estilos.div}>
+                <p className={estilos.paragrafo}>Preços baixos em</p>
+                <h1 className={estilos.titulo}> Óculos de<br/>grau e de sol</h1>
+                <p className={estilos.paragrafo}>Você só encontra aqui</p>
+            </div>
+        </div>
+        </section>
+    )
+}
--- src/componentes/topo/index.jsx
+++ src/componentes/topo/index.jsx
@@ -1,3 +1,18 @@
+import estilos from '@/componentes/topo/topo.module.css';
+import Logo from '../../../public/logo.png'
+import Image from 'next/image';
+import Link from 'next/link';
+
+
 export default function Topo(){
-    return
+    return(
+        <header className={estilos.container}>
+            <Image className={estilos.img_logo} src={Logo} alt='Logo Otica'/>
+            <nav className={estilos.nav}>
+                <Link className={estilos.link} href="#">Produtos</Link>
+                <Link className={estilos.link}  href="#">Sobre</Link>
+                <Link className={estilos.link}  href="#">Contatos</Link>
+            </nav>
+        </header>
+    )
 }
